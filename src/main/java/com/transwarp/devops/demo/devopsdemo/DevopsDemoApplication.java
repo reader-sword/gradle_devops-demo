ac38551831ff0b92310b374525de0993d9d16ecc
flowingsun <18844993660@163.com>
2019-04-11 09:55:54 +0800

    添加启动参数

--- src/main/java/com/transwarp/devops/demo/devopsdemo/DevopsDemoApplication.java
+++ src/main/java/com/transwarp/devops/demo/devopsdemo/DevopsDemoApplication.java
@@ -1,5 +1,6 @@
 package com.transwarp.devops.demo.devopsdemo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 @RestController
@@ -21,7 +21,9 @@ public class DevopsDemoApplication {
     }
 
 
-
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsDemoApplication.class, args);
+    }
 
 
 }
